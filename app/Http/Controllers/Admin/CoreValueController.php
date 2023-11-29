78d3301d09d9aa2741e633b15f1dd21de56f6610
Longkunz <cuvip1325@gmail.com>
2023-11-29 21:28:22 +0700

    [Update] - Update core value admin UI

--- app/Http/Controllers/Admin/CoreValueController.php
+++ app/Http/Controllers/Admin/CoreValueController.php
@@ -13,62 +13,62 @@ class CoreValueController extends Controller
 {
     private $coreValueService;
 
-    public function __construct(CoreValueService $coreValueService){
+    public function __construct(CoreValueService $coreValueService)
+    {
         $this->coreValueService = $coreValueService;
     }
 
-    public function index(){
+    public function index()
+    {
         $viewModel = [];
         $viewModel['coreValue'] = $this->coreValueService->all();
         return view("admin.pages.core.index", $viewModel);
     }
 
-    public function create(Request $request){
-        try{
+    public function store(Request $request)
+    {
+        try {
             $validator = Validator::make($request->all(), [
-                'thumbnail' => [
-                    'required',
-                    'image',
-                    'mimes:png,jpg,jpeg,gif',
-                    'mimetypes:image/jpeg,image/png,image/gif'
-                ],
+                'thumbnail' => 'required',
                 'title' => 'required',
                 'description' => 'required'
             ]);
 
-            if(!$validator->passes()){
-                return response()->json(['errors' => $validator->errors()]);
-            }
-
             $data = $validator->getData();
-            //check thumbnail
-            if($request->file('thumbnail')){
-                $data['thumbnail'] = storeImage(
-                                        $request->file('thumbnail'), 
-                                        'core'
-                                    );
-            }
 
             $this->coreValueService->save($data);
-            return response()->json(['success' => true]);
-        }catch(Exception $e){
-            return response()->json(['error' => $e->getMessage()]);
+            return redirect()->route('admin.core.index')
+                ->with('success', 'Tạo core value thành công!');
+        } catch (Exception $e) {
+            flash()->addError($e->getMessage());
+            return redirect()->back();
         }
     }
 
     /**
      * API: admin/core/update/{id}
      */
-    public function update($id){
-        $core = $this->coreValueService->find($id);
-        if(!$core){
-            return response()->json(['error'=> NOT_FOUND]);
+    public function update(Request $request, $id)
+    {
+        try {
+            $data = $request->validate([
+                'thumbnail' => 'required',
+                'title' => 'required',
+                'description' => 'required'
+            ]);
+
+            $this->coreValueService->update($data, $id);
+            return redirect()->route('admin.core.index')
+                ->with('success', 'Cập nhật core value thành công!');
+        } catch (Exception $e) {
+            flash()->addError($e->getMessage());
+            return redirect()->back();
         }
-        return response()->json(['data' => $core]); 
     }
 
-    public function handleUpdate(Request $request){
-        try{
+    public function handleUpdate(Request $request)
+    {
+        try {
             $validator = Validator::make($request->all(), [
                 'thumbnail' => [
                     'image',
@@ -79,34 +79,46 @@ class CoreValueController extends Controller
                 'description' => 'required'
             ]);
 
-            if(!$validator->passes()){
+            if (!$validator->passes()) {
                 return response()->json(['errors' => $validator->errors()]);
             }
 
             $data = $validator->getData();
             //check thumbnail
-            if($request->file('thumbnail')){
+            if ($request->file('thumbnail')) {
                 $data['thumbnail'] = storeImage(
-                                        $request->file('thumbnail'), 
-                                        'core'
-                                    );
+                    $request->file('thumbnail'),
+                    'core'
+                );
             }
 
             $this->coreValueService->update($data, $request->id);
             return response()->json(['success' => true]);
-        }catch(Exception $e){
+        } catch (Exception $e) {
             return response()->json(['error' => $e->getMessage()]);
         }
     }
 
-    public function destroy($id){
-        try{
+    public function destroy($id)
+    {
+        try {
             $this->coreValueService->delete($id);
             flash()->addSuccess(DELETE_SUCCESS);
             return redirect()->back();
-        }catch(Exception $e){
+        } catch (Exception $e) {
             flash()->addError($e->getMessage());
             return redirect()->back();
         }
+    }
+
+    public function create()
+    {
+        return view('admin.pages.core.create');
+    }
+
+    public function edit($id)
+    {
+        $coreValue = $this->coreValueService->find($id);
+        return view('admin.pages.core.edit', compact('coreValue'));
     }
 }
--- resources/views/admin/pages/core/index.blade.php
+++ resources/views/admin/pages/core/index.blade.php
@@ -1,54 +1,130 @@
 @extends('admin.layouts.master')
 @section('content')
+    <!-- DataTales Example -->
     <div class="container-fluid">
-        <div class="card">
-            <h5 class="card-header">
-                Giá Trị Cốt Lõi
-                <button type="button" class="btn btn-outline-primary rounded-circle" id="btn-add">
-                    <i class="fas fa-plus"></i>
-                </button>
-            </h5>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary float-left">Danh sách giá trị cốt lõi</h6>
+                <a href="{{ route('admin.core.create') }}" class="btn btn-primary btn-sm float-right" data-toggle="tooltip"
+                    data-placement="bottom" title="Add core value"><i class="fas fa-plus"></i> Thêm
+                    mới</a>
+            </div>
             <div class="card-body">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col" style="width:15%">Hình ảnh</th>
-                            <th scope="col" class="w-25">Tiêu đề</th>
-                            <th scope="col" style="width:35%">Mô tả</th>
-                            <th scope="col" class="w-25">Hành động</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($coreValue as $item)
-                            <tr>
-                                <td class="align-middle">
-                                    <img class="w-100" src="{{ asset($item->thumbnail) }}" alt="Hinh anh"/>
-                                </td>
-                                <td class="align-middle">{{ $item->title }}</td>
-                                <td class="align-middle">{{ $item->description }}</td>
-                                <td class="align-middle">
-                                    <div class="d-flex">
-                                        <button type="button" data-id="{{ $item->id }}" class="btn btn-outline-primary w-50 editRow">
-                                            <i class="fas fa-pen"></i>
-                                            <span>Cập nhật</span>
-                                        </button>
-                                        <a type="button" 
-                                            href="{{ route('admin.core.destroy', $item->id) }}"
-                                            class="btn btn-outline-danger w-50 ml-2">
-                                            <i class="fas fa-trash-alt"></i>
-                                            <span>Xoá</span>
-                                        </a>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
-
-                    </tbody>
-                </table>
+                <div class="table-responsive">
+                    @if ($coreValue->isNotEmpty())
+                        <table class="table table-bordered" id="banner-dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th style="width: 2%;">ID</th>
+                                    <th>Tiêu đề</th>
+                                    <th>Nội dung</th>
+                                    <th>Hình ảnh</th>
+                                    <th style="width: 8%;">Thao tác</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($coreValue as $core)
+                                    <tr>
+                                        <td>{{ $core->id }}</td>
+                                        <td>{{ $core->title }}</td>
+                                        <td>{{ $core->description }}</td>
+                                        <td>
+                                            @if ($core->thumbnail)
+                                                <img src="{{ $core->thumbnail }}" class="img-fluid zoom"
+                                                    style="max-width:80px" alt="{{ $core->title }}">
+                                            @else
+                                                <img src="{{ asset('backend/img/thumbnail-default.jpg') }}"
+                                                    class="img-fluid zoom" style="max-width:100%" alt="avatar.png">
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <a href="{{ route('admin.core.edit', $core->id) }}"
+                                                class="btn btn-primary btn-sm float-left mr-1"
+                                                style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip"
+                                                title="edit" data-placement="bottom"><i class="fas fa-edit"></i></a>
+                                            <form method="POST" action="{{ route('admin.core.delete', $core->id) }}">
+                                                @csrf
+                                                @method('delete')
+                                                <button class="btn btn-danger btn-sm dltBtn" data-id={{ $core->id }}
+                                                    style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip"
+                                                    data-placement="bottom" title="Delete"><i
+                                                        class="fas fa-trash-alt"></i></button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @else
+                        <h6 class="text-center">Vui lòng thêm giá trị cốt lõi!</h6>
+                    @endif
+                </div>
             </div>
         </div>
     </div>
-    @include('admin.pages.core.create')
-    @include('admin.pages.core.update')
-    @include('admin.pages.core.script')
 @endsection
+
+@push('styles')
+    <link href="{{ asset('backend/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" />
+    <style>
+        div.dataTables_wrapper div.dataTables_paginate {
+            display: none;
+        }
+
+        .zoom {
+            transition: transform .2s;
+            /* Animation */
+        }
+
+        .zoom:hover {
+            transform: scale(3.2);
+        }
+    </style>
+@endpush
+
+@push('scripts')
+    <!-- Page level plugins -->
+    <script src="{{ asset('backend/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('backend/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{ asset('backend/js/demo/datatables-demo.js') }}"></script>
+    <script>
+        $('#banner-dataTable').DataTable({
+            "columnDefs": [{
+                "orderable": false,
+                "targets": [3, 4, 5]
+            }]
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $('.dltBtn').click(function(e) {
+                var form = $(this).closest('form');
+                var dataID = $(this).data('id');
+                // alert(dataID);
+                e.preventDefault();
+                swal({
+                        title: "Bạn có chắc chắn không?",
+                        text: "Khi bấm xoá, data không thể khôi phục!",
+                        icon: "warning",
+                        buttons: true,
+                        dangerMode: true,
+                    })
+                    .then((willDelete) => {
+                        if (willDelete) {
+                            form.submit();
+                        } else {
+                            swal("Đã hoàn tác!");
+                        }
+                    });
+            })
+        })
+    </script>
+@endpush
