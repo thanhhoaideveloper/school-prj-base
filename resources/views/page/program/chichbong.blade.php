2bffb4707da9a4c8cc06c18b772ca7b78d824115
hoaidev <hoaidev@gmail.com>
2023-12-08 00:04:31 +0700

    page chichbong

---
+++ resources/views/page/program/chichbong.blade.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="{{ asset('chichbong/globals.css') }}" />
+    <link rel="stylesheet" href="{{ asset('chichbong/styleguide.css') }}" />
+    <link rel="stylesheet" href="{{ asset('chichbong/style.css') }}" />
+    <style>
+        .programs-chichbong .content-wrapper {
+            background-image: url('{{ asset('homepage/img/union-shape-1.svg') }}');
+        }
+    </style>
+  </head>
+  <body>
+    <div class="programs-chichbong">
+      <div class="div">
+        <div class="frame">
+          <div class="frame-2">
+            <div class="text-wrapper">Chương trình học</div>
+            <p class="p">
+              A Reggio-Inspired School for children from 1 year to 11 years of age from Nursery, Preschool to Primary.
+            </p>
+          </div>
+          <div class="frame-3">
+            <div class="div-program-b-cu"><div class="text-wrapper-2">Lớp Bồ Câu</div></div>
+            <div class="div-program-s-nu"><div class="text-wrapper-2">Lớp Sẻ Nâu</div></div>
+            <div class="div-program-chch-bng">
+              <div class="content-programs-chichbong">
+                <div class="text-wrapper-3">Lớp Chích Bông</div>
+                <p class="text-wrapper-4">
+                  Provides children aged 0-2 with opportunities for learning through movement, exploration, and
+                  connection. Learning goals for infants focus on movement skills, language development, developmental
+                  milestones, fine motor development, social skills, and the joy of every child.
+                </p>
+                <div class="flexcontainer">
+                  <p class="div-2-chichbong">
+                    <span class="span">Độ tuổi: 0-2 tuổi<br /></span>
+                  </p>
+                  <p class="div-2-chichbong"><span class="span">Sỉ số mỗi lớp: 5</span></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="overlap">
+          <div class="frame-4">
+            <div class="div-3">
+              <div class="frame-5">
+                <div class="text-wrapper-5">Lớp Chích Bông</div>
+                <div class="div-4">
+                  <div class="ghost-button"><div class="th-ng-tin">Môi trường lớp học</div></div>
+                  <div class="ghost-button">
+                    <div class="th-ng-tin-2">Phương pháp giáo dục</div>
+                    <img class="caret-up" src="{{ asset('homepage/img/caretup-4.svg') }}" />
+                  </div>
+                  <div class="ghost-button"><div class="th-ng-tin">Lịch trình một ngày</div></div>
+                  <div class="ghost-button"><div class="th-ng-tin">Học phí</div></div>
+                </div>
+              </div>
+              <div class="frame-6"></div>
+            </div>
+            <div class="container-wrapper">
+              <div class="container">
+                <div class="div-wrapper">
+                  <div class="div-5">
+                    <div class="text-wrapper-6">Môi trường học</div>
+                    <div class="flexcontainer">
+                      <p class="div-2-chichbong">
+                        <span class="span">Example materials and activities include:<br /></span>
+                      </p>
+                      <p class="div-2-chichbong">
+                        <span class="span">Treasure baskets&nbsp;&nbsp; <br /></span>
+                      </p>
+                      <p class="div-2-chichbong">
+                        <span class="span">Fabric matching&nbsp;&nbsp; <br /></span>
+                      </p>
+                      <p class="div-2-chichbong">
+                        <span class="span">Spooning <br /></span>
+                      </p>
+                      <p class="div-2-chichbong">
+                        <span class="span">Pegging <br /></span>
+                      </p>
+                      <p class="div-2-chichbong"><span class="span">Nesting objects </span></p>
+                    </div>
+                    <div class="group">
+                      <div class="div-carousel">
+                        <img class="rectangle" src="{{ asset('homepage/img/rectangle-4204.png') }}" />
+                        <img class="img" src="{{ asset('homepage/img/rectangle-4205.png') }}" />
+                        <img class="rectangle-2" src="{{ asset('homepage/img/rectangle-4206.png') }}" />
+                        <img class="rectangle-3" src="{{ asset('homepage/img/rectangle-4207.png') }}" />
+                        <img class="rectangle-4" src="{{ asset('homepage/img/rectangle-4208.png') }}" />
+                        <img class="rectangle-5" src="{{ asset('homepage/img/rectangle-4209.png') }}" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="div-wrapper">
+                  <div class="div-5">
+                    <div class="text-wrapper-6">Phương pháp giáo dục</div>
+                    <p class="div-2-chichbong">
+                      Infants learn through movement and exploration. Learning goals for infants include healthy
+                      physical development. Through the Infant Curriculum, children develop fine motor, gross motor, and
+                      fundamental movement skills.
+                    </p>
+                    <img class="rectangle-6" src="{{ asset('homepage/img/rectangle-4204-1.png') }}" />
+                  </div>
+                </div>
+                <div class="div-wrapper">
+                  <div class="div-5">
+                    <div class="text-wrapper-6">Lịch trình một ngày</div>
+                    <p class="div-2-chichbong">
+                      Infants learn through movement and exploration. Learning goals for infants include healthy
+                      physical development. Through the Infant Curriculum, children develop fine motor, gross motor, and
+                      fundamental movement skills.
+                    </p>
+                    <img class="rectangle-7" src="{{ asset('homepage/img/rectangle-4204-2.png') }}" />
+                  </div>
+                </div>
+                <div class="div-wrapper">
+                  <div class="div-5">
+                    <div class="text-wrapper-6">Học Phí</div>
+                    <div class="flexcontainer">
+                      <p class="text">
+                        <span class="text-wrapper-7"
+                          >Infants learn through movement and exploration. Learning goals for infants include healthy
+                          physical development. Through the Infant Curriculum, children develop fine motor, gross motor,
+                          and fundamental movement skills.<br
+                        /></span>
+                      </p>
+                      <p class="text"><span class="text-wrapper-8">12.000.000đ/tháng</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="div-footer">
+            <div class="frame-7">
+              <div class="div-logo-address">
+                <img class="logo" src="{{ asset('homepage/img/logo-2.svg') }}" />
+                <p class="text-wrapper-9">
+                  Số 1, Đường Số 14, Khu Phố 5, Phường An Phú, Thành phố Thủ Đức, Thành phố Hồ Chí Minh
+                </p>
+              </div>
+              <div class="contact">
+                <div class="frame-8">
+                  <img class="img-2" src="{{ asset('homepage/img/envelopesimple.svg') }}" />
+                  <div class="text-wrapper-10">info@nido.com.vn</div>
+                </div>
+                <div class="frame-9">
+                  <img class="img-2" src="{{ asset('homepage/img/phone-1.svg') }}" />
+                  <div class="text-wrapper-10">0904 859 864</div>
+                </div>
+              </div>
+            </div>
+            <div class="div-lisence">
+              <div class="copyright">
+                <div class="copyright-by">© Copyright&nbsp;&nbsp;2023&nbsp;&nbsp;by NIDO Inc.</div>
+              </div>
+              <div class="social-media"><img class="facebook-logo" src="{{ asset('homepage/img/facebooklogo-1.svg') }}" /></div>
+            </div>
+          </div>
+        </div>
+        @include("page.partial.header")
+        <div class="frame-10">
+          <div class="img-wrapper"><img class="img-3" src="{{ asset('homepage/img/zalo-png-0.png') }}" /></div>
+          <div class="img-wrapper"><img class="img-3" src="{{ asset('homepage/img/messenger-jpeg-0.png') }}" /></div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
