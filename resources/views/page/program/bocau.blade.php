cb83595f2f12ca57cee48056bd4bbf59771bbf87
Longkunz <cuvip1325@gmail.com>
2023-12-09 03:33:13 +0700

    Fix

---
+++ resources/views/page/program/bocau.blade.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="{{ asset('bocau/globals.css') }}" />
+    <link rel="stylesheet" href="{{ asset('bocau/styleguide.css') }}" />
+    <link rel="stylesheet" href="{{ asset('bocau/style.css') }}" />
+</head>
+
+<body>
+    <div class="programs-bocau">
+        <div class="div">
+            <div class="frame">
+                <div class="overlap-group">
+                    <div class="rectangle"></div>
+                    <div class="frame-2">
+                        <div class="text-wrapper">Chương trình học</div>
+                        <p class="p">
+                            A Reggio-Inspired School for children from 1 year to 11 years of age from Nursery, Preschool
+                            to Primary.
+                        </p>
+                    </div>
+                    <div class="frame-3">
+                        <div class="div-program-card">
+                        </div>
+                        <div class="div-program-card-2">
+                            <div class="content">
+                                <p class="text-wrapper-4">
+                                    The Bo Cau is a structured academics and wellbeing program that prepares children
+                                    for their
+                                    transition to school. It expands on the foundational knowledge fostered by previous
+                                    programs and
+                                    assists in developing confidence, independence, problem-solving, creativity, and
+                                    real-life skills.
+                                </p>
+                                <div class="flexcontainer">
+                                    <p class="div-2">
+                                        <span class="span">Độ tuổi: 3-6 tuổi<br /></span>
+                                    </p>
+                                    <p class="div-2"><span class="span">Sỉ số mỗi lớp: 20</span></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="div-program-card-3">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="div-footer">
+                <div class="frame-4">
+                    <div class="div-logo-address">
+                        <img class="logo" src="{{ asset('bocau/img/logo-3.svg') }}" />
+                        <p class="text-wrapper-5">
+                            Số 1, Đường Số 14, Khu Phố 5, Phường An Phú, Thành phố Thủ Đức, Thành phố Hồ Chí Minh
+                        </p>
+                    </div>
+                    <div class="contact">
+                        <div class="frame-5">
+                            <img class="img" src="{{ asset('bocau/img/envelopesimple.svg') }}" />
+                            <div class="text-wrapper-6">info@nido.com.vn</div>
+                        </div>
+                        <div class="frame-6">
+                            <img class="img" src="{{ asset('bocau/img/phone-1.svg') }}" />
+                            <div class="text-wrapper-6">0904 859 864</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="div-lisence">
+                    <div class="copyright">
+                        <div class="copyright-by">© Copyright&nbsp;&nbsp;2023&nbsp;&nbsp;by NIDO Inc.</div>
+                    </div>
+                    <div class="social-media"><img class="facebook-logo"
+                            src="{{ asset('bocau/img/facebooklogo-1.svg') }}" /></div>
+                </div>
+            </div>
+            <div class="div-3">
+                <div class="frame-7">
+                    <div class="text-wrapper-7">Lớp Bồ Câu</div>
+                    <div class="div-4">
+                        <div class="ghost-button">
+                            <div class="th-ng-tin">Môi trường lớp học</div>
+                        </div>
+                        <div class="ghost-button">
+                            <div class="th-ng-tin-2">Phương pháp giáo dục</div>
+                            <img class="caret-up" src="{{ asset('bocau/img/caretup-5.svg') }}" />
+                        </div>
+                        <div class="ghost-button">
+                            <div class="th-ng-tin">Chương trình tiếng Anh</div>
+                        </div>
+                        <div class="ghost-button">
+                            <div class="th-ng-tin">Lịch trình một ngày</div>
+                        </div>
+                        <div class="ghost-button">
+                            <div class="th-ng-tin">Học phí</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="frame-8"></div>
+            </div>
+            <div class="container-wrapper">
+                <div class="container">
+                    <div class="div-wrapper">
+                        <div class="div-5">
+                            <div class="text-wrapper-8">Môi trường học</div>
+                            <div class="flexcontainer">
+                                <p class="div-2">
+                                    <span class="span">Example materials and activities include:<br /></span>
+                                </p>
+                                <p class="div-2">
+                                    <span class="span">Treasure baskets&nbsp;&nbsp; <br /></span>
+                                </p>
+                                <p class="div-2">
+                                    <span class="span">Fabric matching&nbsp;&nbsp; <br /></span>
+                                </p>
+                                <p class="div-2">
+                                    <span class="span">Spooning <br /></span>
+                                </p>
+                                <p class="div-2">
+                                    <span class="span">Pegging <br /></span>
+                                </p>
+                                <p class="div-2"><span class="span">Nesting objects </span></p>
+                            </div>
+                            <div class="div-carousel">
+                                <img class="rectangle-2" src="{{ asset('bocau/img/rectangle-4204-3.png') }}" />
+                                <img class="rectangle-3" src="{{ asset('bocau/img/rectangle-4205.png') }}" />
+                                <img class="rectangle-3" src="{{ asset('bocau/img/rectangle-4206.png') }}" />
+                                <img class="rectangle-4" src="{{ asset('bocau/img/rectangle-4207.png') }}" />
+                                <img class="rectangle-3" src="{{ asset('bocau/img/rectangle-4208.png') }}" />
+                                <img class="rectangle-3" src="{{ asset('bocau/img/rectangle-4209.png') }}" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="div-wrapper">
+                        <div class="div-5">
+                            <div class="text-wrapper-8">Phương pháp giáo dục</div>
+                            <p class="div-2">
+                                Infants learn through movement and exploration. Learning goals for infants include
+                                healthy physical
+                                development. Through the Infant Curriculum, children develop fine motor, gross motor,
+                                and fundamental
+                                movement skills.
+                            </p>
+                        </div>
+                    </div>
+                    <img class="rectangle-2" src="{{ asset('bocau/img/rectangle-4204-2.png') }}" />
+                    <div class="div-wrapper">
+                        <div class="div-5">
+                            <div class="text-wrapper-8">Chương trình Tiếng Anh</div>
+                            <p class="div-2">
+                                Infants learn through movement and exploration. Learning goals for infants include
+                                healthy physical
+                                development. Through the Infant Curriculum, children develop fine motor, gross motor,
+                                and fundamental
+                                movement skills.
+                            </p>
+                            <img class="rectangle-2" src="{{ asset('bocau/img/rectangle-4204-1.png') }}" />
+                        </div>
+                    </div>
+                    <div class="div-wrapper">
+                        <div class="div-5">
+                            <div class="text-wrapper-8">Lịch trình một ngày</div>
+                            <p class="div-2">
+                                Infants learn through movement and exploration. Learning goals for infants include
+                                healthy physical
+                                development. Through the Infant Curriculum, children develop fine motor, gross motor,
+                                and fundamental
+                                movement skills.
+                            </p>
+                            <img class="rectangle-5" src="{{ asset('bocau/img/rectangle-4204.png') }}" />
+                        </div>
+                    </div>
+                    <div class="div-wrapper">
+                        <div class="div-5">
+                            <div class="text-wrapper-8">Học Phí</div>
+                            <div class="flexcontainer">
+                                <p class="text">
+                                    <span class="text-wrapper-9">Infants learn through movement and exploration.
+                                        Learning goals for infants include healthy
+                                        physical development. Through the Infant Curriculum, children develop fine
+                                        motor, gross motor, and
+                                        fundamental movement skills.<br /></span>
+                                </p>
+                                <p class="text"><span class="text-wrapper-10">12.000.000đ/tháng</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="overlap-wrapper">
+                <div class="overlap">
+                    <div class="content-2">
+                        <div class="div-6">
+                            <div class="ghost-button-2">
+                                <div class="th-ng-tin">Về chúng tôi</div>
+                            </div>
+                            <div class="ghost-button-2">
+                                <div class="th-ng-tin-3">Chương trình học</div>
+                                <img class="caret-up" src="{{ asset('bocau/img/caretup-4.svg') }}" />
+                            </div>
+                        </div>
+                        <img class="logo-2" src="{{ asset('bocau/img/logo.png') }}" />
+                        <div class="div-6">
+                            <div class="ghost-button-2">
+                                <p class="th-ng-tin">Tin tức &amp; Hình ảnh</p>
+                            </div>
+                            <div class="ghost-button-2">
+                                <div class="th-ng-tin">Liên hệ</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
