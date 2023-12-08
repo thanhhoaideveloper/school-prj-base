fc8523be3f20173d88ba6eafc52a1c91daa5606b
Longkunz <cuvip1325@gmail.com>
2023-12-09 01:05:14 +0700

    [Fix] - Fix responsive homepage

---
+++ resources/views/page/partial/footer.blade.php
@@ -0,0 +1,36 @@
+<div class="div-footer">
+    <div class="div-lisence">
+        <div class="copyright">
+            <div class="copyright-by">© Copyright&nbsp;&nbsp;2023&nbsp;&nbsp;by NIDO Ltd.</div>
+        </div>
+        <div class="social-media">
+            <img class="img" src="img/facebooklogo.svg" />
+            <img class="img" src="img/instagramlogo-1.svg" />
+            <img class="img" src="img/tiktoklogo-1.svg" />
+            <img class="img" src="img/youtubelogo-1.svg" />
+        </div>
+    </div>
+    <div class="address">
+        <p class="text-wrapper-15">
+            Số 1, Đường Số 14, Khu Phố 5, Phường An Phú, Thành phố Thủ Đức, Thành phố Hồ Chí Minh
+        </p>
+    </div>
+    <div class="contact">
+        <div class="frame-3">
+            <img class="img" src="{{ asset('homepage/img/envelopesimple-1.svg') }}" />
+            <div class="text-wrapper-16">info@nido.com.vn</div>
+        </div>
+        <div class="frame-3">
+            <img class="img" src="{{ asset('homepage/img/phone.svg') }}" />
+            <div class="text-wrapper-16">0904 859 864</div>
+        </div>
+    </div>
+    <img class="logo" src="{{ asset('homepage/img/logo-1.svg') }}" />
+</div>
+
+
+
+<div class="frame-19">
+    <div class="img-wrapper"><img class="img-2" src="{{ asset('homepage/img/zalo-png-0.png') }}" /></div>
+    <div class="img-wrapper"><img class="img-2" src="{{ asset('homepage/img/messenger-jpeg-0.png') }}" /></div>
+</div>
--- resources/views/page/partial/header.blade.php
+++ resources/views/page/partial/header.blade.php
@@ -1,14 +1,62 @@
-<div class="div-nav" style="width: 100%">
-    <div class="content-wrapper" style="margin: 0 auto; left: 0; top: 10px">
+<div class="div-6">
+    <div class="content-wrapper">
+        <!-- // nav  -->
+        <div class="icon-bars">
+            <nav class="menu--left" role="navigation">
+                <div class="menuToggle">
+                    <input type="checkbox" />
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <div class="menuItem">
+                        <ul class="menuItem-main">
+                            <li>
+                                <a href="{{ route('about') }}">Về chúng tôi</a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <div class="th-ng-tin" style="float: left; margin-right: .5rem">
+                                        Chương trình học
+                                    </div>
+                                    <img class="caret-up" src="{{ asset('homepage/img/caretup.svg') }}" />
+                                    <ul class="sub-program">
+                                        <li> Lớp Chích Bông </li>
+                                        <li> Lớp Sẻ Nâu </li>
+                                        <li> Lớp Bồ Câu </li>
+                                    </ul>
+                                </a>
+                            </li>
+                            <li><a href="#">Tin tức &amp; Hình ảnh</a></li>
+                            <li>
+                                <a href="{{ route('contact') }}">
+                                    <div class="th-ng-tin" style="float: left">Liên hệ</div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class="content-border"> </div>
         <div class="content-3">
-            <div class="div-6">
-                <a href="/ve-chung-toi" class="ghost-button"><div class="th-ng-tin">Về chúng tôi</div></a>
-                <a href="/chuong-trinh-hoc" class="ghost-button"><div class="th-ng-tin">Chương trình học</div></a>
+            <div class="div-7">
+                <div class="ghost-button">
+                    <div class="th-ng-tin">Về chúng tôi</div>
+                </div>
+                <div class="ghost-button">
+                    <div class="th-ng-tin">Chương trình học</div>
+                    <img class="caret-up" src="{{ asset('homepage/img/caretup.svg') }}" />
+                </div>
             </div>
-            <a href="{{route('home')}}"><img class="logo-2" src="{{asset('home-page-2/img/logo-3.png')}}" /></a>
-            <div class="div-6">
-                <a href="/bai-viet" class="ghost-button"><div class="th-ng-tin">Tin tức &amp; Hình ảnh</div></a>
-                <a href="/lien-he" class="ghost-button"><div class="th-ng-tin">Liên hệ</div></a>
+            <img class="logo-2" src="{{ asset('homepage/img/logo-3.png') }}" />
+            <div class="div-7">
+                <div class="ghost-button">
+                    <p class="th-ng-tin">Tin tức &amp; Hình ảnh</p>
+                </div>
+                <div class="ghost-button">
+                    <div class="th-ng-tin">Liên hệ</div>
+                </div>
             </div>
         </div>
     </div>
